f47f302bdf7b011738bc5f8a1201c21b139fc9dc
Ahmed Heggy <ahmedheggy@pursuit.org>
2023-08-13 18:14:38 -0400

    update the url for the citie images

--- db/seed.sql
+++ db/seed.sql
@@ -1,13 +1,13 @@
 \c cities_dev;
 
 INSERT INTO cities (name, country,population, url,is_favorite) VALUES
-('New York', 'U.S.A.', 8804190, 'https://ibb.co/hVGr5NZ', true),
-('Sydney', 'Australia', 5259764, 'https://ibb.co/Vjr9638', false),
-('London', 'England', 8799800, 'https://ibb.co/j3KSRpd', true),
-('Cape Town', 'South African', 4710000, 'https://ibb.co/fQ7Vdmn', true),
-('Barcelona', 'Spain', 1620000, 'https://ibb.co/hcP5fMy', true),
-('İstanbul', 'Turkey', 15460000, 'https://ibb.co/Cz4shkW', true),
-('Rome', 'Italy', 4355725, 'https://ibb.co/ctL4QYB', true),
-('Kyoto', 'Japan', 1475000, 'https://ibb.co/0s8V116', true),
-('Dubai', 'United Arab Emirates', 3490000, 'https://ibb.co/QPrNm70', true),
-('Berlin', 'Germany', 3850809, 'https://ibb.co/sQMnYhr', true)
+('New York', 'U.S.A.', 8804190, 'https://fullsuitcase.com/wp-content/uploads/2022/05/One-day-in-New-York-USA-NYC-day-trip-itinerary.jpg', true),
+('Sydney', 'Australia', 5259764, 'https://travel.home.sndimg.com/content/dam/images/travel/fullset/2013/12/04/7f/sydney-opera-house-2.rend.hgtvcom.616.462.suffix/1491592110293.jpeg', false),
+('London', 'England', 8799800, 'https://a.cdn-hotels.com/gdcs/production29/d1870/6a5ec560-bb25-11e8-970b-0242ac110006.jpg?impolicy=fcrop&w=800&h=533&q=medium', true),
+('Cape Town', 'South African', 4710000, 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/10/f6/3b/99/bo-kaap.jpg?w=600&h=400&s=1', true),
+('Barcelona', 'Spain', 1620000, 'https://a.cdn-hotels.com/gdcs/production132/d1648/561ec893-e380-439b-a977-b6ebbc344ebc.jpg', true),
+('İstanbul', 'Turkey', 15460000, 'https://media.cnn.com/api/v1/images/stellar/prod/230208113026-01-turkey-safe-to-travel-istanbul.jpg?c=3x2', true),
+('Rome', 'Italy', 4355725, 'https://cdn.britannica.com/16/99616-050-72CD201A/Colosseum-Rome.jpg', true),
+('Kyoto', 'Japan', 1475000, 'https://www.nationsonline.org/gallery/Japan/Kiyomizu-dera-Kyoto.jpg', true),
+('Dubai', 'United Arab Emirates', 3490000, 'https://theplanetd.com/images/things-to-do-in-dubai-burj.jpg', true),
+('Berlin', 'Germany', 3850809, 'https://cdn.britannica.com/39/6839-050-27891400/Brandenburg-Gate-Berlin.jpg', true)
