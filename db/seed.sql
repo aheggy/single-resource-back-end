7d2a862512921c7bcc9734dbacb51ce64099be50
Ahmed Heggy <ahmedheggy@pursuit.org>
2023-08-13 09:39:15 -0400

    update seed.sql file

--- db/seed.sql
+++ db/seed.sql
@@ -2,6 +2,12 @@
 
 INSERT INTO cities (name, country,population, url,is_favorite) VALUES
 ('New York', 'U.S.A.', 8804190, 'https://ibb.co/hVGr5NZ', true),
-('New York', 'U.S.A.', 8804190, 'https://ibb.co/hVGr5NZ', true),
-('New York', 'U.S.A.', 8804190, 'https://ibb.co/hVGr5NZ', true),
-('New York', 'U.S.A.', 8804190, 'https://ibb.co/hVGr5NZ', true);
+('Sydney', 'Australia', 5259764, 'https://ibb.co/Vjr9638', false),
+('London', 'England', 8799800, 'https://ibb.co/j3KSRpd', true),
+('Cape Town', 'South African', 4710000, 'https://ibb.co/fQ7Vdmn', true),
+('Barcelona', 'Spain', 1620000, 'https://ibb.co/hcP5fMy', true),
+('İstanbul', 'Turkey', 15460000, 'https://ibb.co/Cz4shkW', true),
+('Rome', 'Italy', 4355725, 'https://ibb.co/ctL4QYB', true),
+('Kyoto', 'Japan', 1475000, 'https://ibb.co/0s8V116', true),
+('Dubai', 'United Arab Emirates', 3490000, 'https://ibb.co/QPrNm70', true),
+('Berlin', 'Germany', 3850809, 'https://ibb.co/sQMnYhr', true),
